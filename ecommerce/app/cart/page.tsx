7e877baa3ae0b467feb123eee02c22f8b335123a
Brecka <Sarbreckb@yahoo.com>
2025-10-31 00:16:41 +0000

    save progress

---
+++ ecommerce/app/cart/page.tsx
@@ -0,0 +1,4 @@
+export default function CartPage(){
+return <h1>Check Out</h1>
+
+}
--- ecommerce/app/products/page.tsx
+++ ecommerce/app/products/page.tsx
@@ -1,6 +1,7 @@
 export default function ProductsPage(){
-return 
-
-<h1>Products</h1>
+    return <>
+        <h1>Products</h1> 
+        <h1>Products</h1>
+        </>
 
 }
