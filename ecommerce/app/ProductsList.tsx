630d3ae56bc868ded2f0dcf1263b225435c01fd1
Brecka <Sarbreckb@yahoo.com>
2025-10-31 05:01:55 +0000

    save progress

--- ecommerce/app/ProductsList.tsx
+++ ecommerce/app/ProductsList.tsx
@@ -6,12 +6,11 @@ export default function ProductsList({products}:{products: Product[]}){
         <div>
             {products.map(product =>(
                 <div key = {product.id}>
-                    <Image src ={product.imageUrl} alt ="Product image" width= {150} height = {150}/>
+                    <Image src ={'/'+ product.imageUrl} alt ="Product image" width= {150} height = {150}/>
                     <h2>{product.name}</h2>
                     <p>${product.price}</p>
                 </div>
             ))}
         </div>
-
     )
 }
